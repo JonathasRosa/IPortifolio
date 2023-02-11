4248c3aa5de56a57d11d4be9ccd1b204293807fb
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-02-11 09:58:17 -0300

    Update Portfólio com Bootstrap

--- forms/contact.php
+++ forms/contact.php
@@ -15,7 +15,7 @@
     die( 'Unable to load the "PHP Email Form" Library!');
   }
 
-  $contact = new PHP_Email_Form;
+  $contact = new gmail_Form;
   $contact->ajax = true;
   
   $contact->to = $receiving_email_address;
@@ -24,14 +24,13 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'jonathasrosa85@gmail.com',
+    'username' => 'Jonathas Rosa',
+    'password' => '123456',
     'port' => '587'
   );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
