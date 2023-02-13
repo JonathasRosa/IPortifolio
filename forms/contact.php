31787de8e6454b62efb809ea52104e30b7beb432
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-02-13 10:35:33 -0300

    Update Portfólio with bootstrap

--- forms/contact.php
+++ forms/contact.php
@@ -5,36 +5,28 @@
   * The library should be uploaded to: vendor/php-email-form/php-email-form.php
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
-
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'contact@example.com';
-
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
   }
-
   $contact = new gmail_Form;
   $contact->ajax = true;
-  
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
-
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  
   $contact->smtp = array(
     'host' => 'jonathasrosa85@gmail.com',
     'username' => 'Jonathas Rosa',
     'password' => '123456',
     'port' => '587'
   );
-
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
-
   echo $contact->send();
 ?>
